d3458788ca83e5f7d83d4b7577e14641c456bdd8
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-16 05:39:47 +0100

    listener successfully connected to nats

---
+++ src/listener.ts
@@ -0,0 +1,11 @@
+import nats from 'node-nats-streaming';
+
+const stan = nats.connect('ticketing', '123', {
+  url: 'http://localhost:4222'
+});
+
+stan.on('connect', () => {
+  console.log('Listener coonnected to NATS');
+
+  
+})
