d8a0f17f6ed9cbbb7df0cec11cc2d3815ecd2c9b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-22 06:41:08 +0100

    created a Listener class

--- src/listener.ts
+++ src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
@@ -11,9 +11,16 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex') , {
 stan.on('connect', () => {
   console.log('Listener coonnected to NATS');
 
+  stan.on('close', () => {
+    console.log('NATS connection close!')
+    process.exit();
+  });
+
   const options = stan
     .subscriptionOptions()
-    .setManualAckMode(true);
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('listener-svc');
   const subscription = stan.subscribe(
     'ticket:created', 
     'listener-queue-group',
@@ -30,4 +37,53 @@ stan.on('connect', () => {
 
     msg.ack();
   });
-})
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  
+  private client: Stan;
+  private ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName)
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(
+        `Message received: ${this.subject} / ${this.queueGroupName}`
+      );
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf-8'))
+  }
+}
